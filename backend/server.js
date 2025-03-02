963c23628717da465c2c82313868e4ec24a6db37
aditya <176797545+sk-adi@users.noreply.github.com>
2025-03-03 00:25:59 +0530

    correct typo mistake

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 
 app.use(
     cors({
-        origin:["http://localhost:5173","http://127.0.0.1:5173,https://url-one-flax.vercel.app/"],
+        origin:["http://localhost:5173","http://127.0.0.1:5173,https://url-one-flax.vercel.app"],
         methods:["GET","POST","PUT","DELETE"],
         credentials:true,
     })
