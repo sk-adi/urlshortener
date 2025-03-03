4fc11b5d96e99783dd8cb944490e32312aead7aa
aditya <176797545+sk-adi@users.noreply.github.com>
2025-03-03 13:44:27 +0530

    bix fixed

--- frontend/src/pages/Redirect.jsx
+++ frontend/src/pages/Redirect.jsx
@@ -1,52 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { redirectUser } from '../api/redirectApi'
-
-
-
+import React, { useEffect, useState } from "react";
+import { replace, useNavigate, useParams } from "react-router-dom";
+import { redirectUser } from "../api/redirectApi";
 
 function Redirect() {
-    const [success, Setsuccess] = useState()
-    const internalRoutes = ['login', 'register', 'dashboard']
-    const navigate = useNavigate()
-    const { code } = useParams()
-    useEffect(() => {
-        
+  const [success, Setsuccess] = useState();
+  const internalRoutes = new Set(["login", "register", "dashboard"]);
+  const navigate = useNavigate();
+  const { code } = useParams();
+  useEffect(() => {
+    if (internalRoutes.has(code)) {
+      navigate(`/${code}`, { replace: true });
+      return;
+    }
 
-        const fetchUrlAndRedirect = async () => {
+    const fetchUrlAndRedirect = async () => {
+      try {
+        const response = await redirectUser(code);
 
-
-
-            try {
-                const response = await redirectUser(code)
-
-                if (response.redirectUrl) {
-                }
-                else {
-                    console.log("Url Not Found")
-                    Setsuccess("Error : 404 ! Url Not Found")
-                }
-
-            } catch (error) {
-                console.log(error)
-
-            }
+        if (response.redirectUrl) {
+        } else {
+          console.log("Url Not Found");
+          Setsuccess("Error : 404 ! Url Not Found");
         }
-        if (!internalRoutes.includes(code)) {
-            //navigate(`/${code}`)
-            fetchUrlAndRedirect()
-        
-        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUrlAndRedirect();
+  }, [code]);
 
-
-    }, [code])
-
-    return (
-        <>
-            <h1>redirecting...</h1>
-            <h1>{success}</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>redirecting...</h1>
+      <h1>{success}</h1>
+    </>
+  );
 }
 
-export default Redirect
+export default Redirect;
