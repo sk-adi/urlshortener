9f24700956eddbd67bacb2c12b7e0eadd5320db1
aditya <176797545+sk-adi@users.noreply.github.com>
2025-03-04 20:00:44 +0530

    testing

--- frontend/src/pages/Redirect.jsx
+++ frontend/src/pages/Redirect.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect } from "react";
-import { redirectUser } from "../api/redirectApi";
-import { useNavigate, useParams } from "react-router-dom";
+// import React, { useEffect } from "react";
+// import { redirectUser } from "../api/redirectApi";
+// import { useNavigate, useParams } from "react-router-dom";
 
-function Redirect() {
-  const internalRoutes = new Set(["login", "register", "dashboard", "routehistory", "user"]);
-  const { code } = useParams();
-  //const navigate = useNavigate();
+// function Redirect() {
+//   const internalRoutes = new Set(["login", "register", "dashboard", "routehistory", "user"]);
+//   const { code } = useParams();
+//   //const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log(`redirect running and this is code ${code}`)
-    if (internalRoutes.has(code)) {
-      console.log(`if code run inside useffect`)
-      navigate(`/${code}`, { replace: true });
-      return; // Prevents further execution
-    }
-    else{
-      redirectUser(code);
-    }
+//   useEffect(() => {
+//     console.log(`redirect running and this is code ${code}`)
+//     if (internalRoutes.has(code)) {
+//       console.log(`if code run inside useffect`)
+//       navigate(`/${code}`, { replace: true });
+//       return; // Prevents further execution
+//     }
+//     else{
+//       redirectUser(code);
+//     }
 
-    // Call backend for external routes
-    console.log(`calling backend`)
+//     // Call backend for external routes
+//     console.log(`calling backend`)
 
-  }, [code, navigate]); // Runs only when `code` changes
+//   }, [code, navigate]); // Runs only when `code` changes
 
-  return <div>Redirecting...</div>;
-}
+//   return <div>Redirecting...</div>;
+// }
 
-export default Redirect;
+// export default Redirect;
