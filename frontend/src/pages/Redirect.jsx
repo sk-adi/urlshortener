22bbef024daf9da7fd7f48c3522a884e879991b9
aditya <176797545+sk-adi@users.noreply.github.com>
2025-03-04 19:52:57 +0530

    testing

--- frontend/src/pages/Redirect.jsx
+++ frontend/src/pages/Redirect.jsx
@@ -8,14 +8,19 @@ function Redirect() {
   const navigate = useNavigate();
 
   useEffect(() => {
+    console.log(`redirect running and this is code ${code}`)
     if (internalRoutes.has(code)) {
+      console.log(`if code run inside useffect`)
       navigate(`/${code}`, { replace: true });
       return; // Prevents further execution
+    }
+    else{
+      redirectUser(code);
     }
 
     // Call backend for external routes
     console.log(`calling backend`)
-    redirectUser(code);
+
   }, [code, navigate]); // Runs only when `code` changes
 
   return <div>Redirecting...</div>;
