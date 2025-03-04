be57954403dd0653dfa85f3eec44a4a1f136e202
aditya <176797545+sk-adi@users.noreply.github.com>
2025-03-04 14:35:27 +0530

    fix the bug

--- frontend/src/pages/Redirect.jsx
+++ frontend/src/pages/Redirect.jsx
@@ -7,16 +7,15 @@ function Redirect() {
   const { code } = useParams();
   const navigate = useNavigate();
 
-  useEffect(() => {
     if (internalRoutes.has(code)) {
       navigate(`/${code}`, { replace: true });
       return; // ⬅ Stop execution here
     }
-
-    // If not an internal route, call the redirect function
-    redirectUser(code);
-  }, [code, navigate]);
-
+    else{
+      useEffect(()=>{
+        redirectUser(code)
+      })
+    }
   return <div>Redirecting...</div>;
 }
 
