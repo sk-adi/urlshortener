7ef7cf33612ce031824b5b0848180166898cca34
aditya <176797545+sk-adi@users.noreply.github.com>
2025-03-04 20:06:14 +0530

    testing

--- frontend/src/api/redirectApi.js
+++ frontend/src/api/redirectApi.js
@@ -1,18 +1,17 @@
-import axios from "axios";
+// import axios from "axios";
 
-const api_url=import.meta.env.VITE_API_URL;
+// const api_url=import.meta.env.VITE_API_URL;
 
 
-const redirectUser = async (theCode) => {
-  console.log("redirect js is being called")
-  try {
-    const response = await axios.get(`${api_url}/api/url/${theCode}`);
-    console.log(response.data)
-    return response.data;
-    return null
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const redirectUser = async (theCode) => {
+//   console.log("redirect js is being called")
+//   try {
+//     const response = await axios.get(`${api_url}/api/url/${theCode}`);
+//     console.log(response.data)
+//     return response.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-export { redirectUser };
+// export { redirectUser };
